10ce53412ff43a3109366602a2ba08205e27a0e2
Nicolly <Yuribonholdt2@gmail.com>
2022-02-24 17:02:38 -0300

    Inicio do projeto imprimindo meu nome com a classe Scanner

---
+++ src/aula1/Aula1.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aula1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author IFSC
+ */
+public class Aula1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    Scanner leia = new Scanner (System.in);
+    
+    System.out.print("Escreva seu nome: ");
+    String nome = leia.next();
+    System.out.println(nome+" seja bem vinda ao GiHub!");
+            
+
+                
+    }
+    
+}
